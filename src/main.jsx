7903271d9c7277fe78f8057a7e528dfb66f7c325
Nathan <nathan.walter@ynov.com>
2025-10-02 16:53:16 +0200

    Adding

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import * as Sentry from "@sentry/react"
-import { BrowserTracing } from "@sentry/tracing"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import * as Sentry from "@sentry/react";
 
-// Init Sentry
 Sentry.init({
   dsn: "https://072160ba08d54ae203c7a271fa7bad78@o4510120303460352.ingest.de.sentry.io/4510120315715664",
-  integrations: [new BrowserTracing()],
+  integrations: [new Sentry.BrowserTracing()],
   tracesSampleRate: 1.0,
   sendDefaultPii: true,
-})
+});
 
-// Monte l'app
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
